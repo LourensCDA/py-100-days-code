66cc9186de7b38b51891ac69ef789d80ba3cf736
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2025-11-18 17:45:56 +0200

    Added day1 project Band Name Generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,7 @@
+# Project Day 1 - Band Name Generatir
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+
+# print band name
+print(f"Your band name could be {city} {pet}")
--- day1/task.py
+++ day1/task.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# "" are strings
+# \n is a new line character
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + " " + "Angela")
+# learned this to use as alternative to variable
+print(f"Hello {input("What is your name? ")}!")
+
+name = input("Alt: What is your name? ")
+print(f"Hello {name} it is {len(name)} characters long!")
