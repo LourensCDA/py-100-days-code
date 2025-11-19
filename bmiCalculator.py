ee9e8ba9504ff588aa403621eed72366927ed823
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2025-11-19 21:18:07 +0200

    added bmi calc

---
+++ bmiCalculator.py
@@ -0,0 +1,8 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = float(weight) / (float(height) ** 2)
+
+print(bmi)
---
+++ day2/task.py
@@ -0,0 +1,17 @@
+# first char
+print("Hello"[0])
+# last char
+print("Hello"[-1])
+
+# large integers remember this
+print(123_456_789)
+
+len("1234")
+print(type(123.56))
+
+print(6 / 2)
+# trunc decimals
+print(6 // 2)
+
+# exponent
+print(2**3)
