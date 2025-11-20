2f491feab919cbdef15e33e54e38618bede1b96a
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2025-11-20 19:43:39 +0200

    added bmi calc

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -4,5 +4,7 @@ weight = input("enter your weight in kg: ")
 # Write your code here.
 # Calculate the bmi using weight and height.
 bmi = float(weight) / (float(height) ** 2)
-
 print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi, 2))
