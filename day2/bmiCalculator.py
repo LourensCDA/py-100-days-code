d3016916453778e0d00218164f2a90ad2f08688d
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2025-12-02 17:36:23 +0200

    updated

--- day2/bmiCalculator.py
+++ day2/bmiCalculator.py
@@ -8,3 +8,9 @@ print(bmi)
 print(int(bmi))
 print(round(bmi))
 print(round(bmi, 2))
+if bmi < 18.5:
+    print("underweight")
+elif bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
---
+++ day3/oddOrEvven.py
@@ -0,0 +1,7 @@
+# test if number is odd or even
+getNumber = int(input("Enter a number: "))
+
+if getNumber % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
