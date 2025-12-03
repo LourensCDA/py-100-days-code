430a025d103c4b95637f4fd4626fe3902865b4e9
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2025-12-03 19:34:40 +0200

    completed task

---
+++ day4/headsOrTails.py
@@ -0,0 +1,8 @@
+import random
+
+pickNumber = random.randint(0, 1)
+
+if pickNumber == 0:
+    print("Heads")
+else:
+    print("Tails")
---
+++ day4/task.py
@@ -0,0 +1,10 @@
+import random
+
+randomInteger = random.randint(1, 10)
+print(randomInteger)
+
+randomFloat_0210 = random.random() * 10
+print(randomFloat_0210)
+
+randomFloat = random.uniform(1, 10)
+print(randomFloat)
