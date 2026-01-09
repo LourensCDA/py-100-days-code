23544c4b4b73ef61bf9b4ea7535d4106736f44f6
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2026-01-09 15:05:38 +0200

    task add timmy the turtle

---
+++ day16/task.py
@@ -0,0 +1,18 @@
+# import anotherModule
+
+# print(anotherModule.anotherVariable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+myScreen = Screen()
+print(myScreen.canvheight)
+
+myScreen.exitonclick()
