90fdda88aab8f3bc4be967c15c028645d741d756
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2026-01-25 21:38:55 +0200

    dashed line

--- day18/main.py
+++ day18/main.py
@@ -8,9 +8,16 @@ timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
 
 # draw a square
-for i in range(1, 5):
-    timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(100)
+# for _ in range(4):
+#     timmy_the_turtle.right(90)
+#     timmy_the_turtle.forward(100)
+
+# draw dashed line
+for _ in range(15):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
 
 
 screen.exitonclick()
