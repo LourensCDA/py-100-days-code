85403b3891ce5c338165dfe7dc5274ce1554397b
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2026-01-26 20:10:21 +0200

    added random walk

--- day18/main.py
+++ day18/main.py
@@ -5,7 +5,7 @@ timmy_the_turtle = Turtle()
 
 screen = Screen()
 
-colors_names = ["red", "green", "blue", "cyan", "magenta", "yellow", "black", "white"]
+colors_names = ["red", "green", "blue", "cyan", "magenta", "yellow", "black"]
 
 
 def drawShape(sides):
@@ -16,8 +16,17 @@ def drawShape(sides):
     timmy_the_turtle.color(random.choice(colors_names))
 
 
+def randomWalk(direction):
+
+    timmy_the_turtle.right(direction)
+    timmy_the_turtle.forward(50)
+    timmy_the_turtle.color(random.choice(colors_names))
+
+
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
+timmy_the_turtle.pensize(15)
+timmy_the_turtle.speed("fast")
 
 # draw a square
 # for _ in range(4):
@@ -32,8 +41,12 @@ timmy_the_turtle.color("green")
 #     timmy_the_turtle.pendown()
 
 
-for i in range(3, 11):
-    drawShape(i)
+# for i in range(3, 11):
+#     drawShape(i)
+numberRange = [i for i in range(5, 30)]
 
+direction = [0, 90, 180, 270]
+for _ in range(random.choice(numberRange)):
+    randomWalk(random.choice(direction))
 
 screen.exitonclick()
