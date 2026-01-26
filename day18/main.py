9093e67b1f49bb79918d673637c8dd6651da3720
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2026-01-26 19:57:40 +0200

    draw shapes

--- day18/main.py
+++ day18/main.py
@@ -1,8 +1,20 @@
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
 
 screen = Screen()
+
+colors_names = ["red", "green", "blue", "cyan", "magenta", "yellow", "black", "white"]
+
+
+def drawShape(sides):
+    angle = 360 / sides
+    for _ in range(sides):
+        timmy_the_turtle.left(angle)
+        timmy_the_turtle.forward(100)
+    timmy_the_turtle.color(random.choice(colors_names))
+
 
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
@@ -13,11 +25,15 @@ timmy_the_turtle.color("green")
 #     timmy_the_turtle.forward(100)
 
 # draw dashed line
-for _ in range(15):
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.pendown()
+# for _ in range(15):
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.pendown()
+
+
+for i in range(3, 11):
+    drawShape(i)
 
 
 screen.exitonclick()
